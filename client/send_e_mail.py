71eb7e21cfaddda982e5077714485a16e62c5f6a
hirasawa <hirasawa@mabc.ac.jp>
2025-10-16 12:56:20 +0900

    pywin32ライブラリを利用したメール送信

--- client/send_e_mail.py
+++ client/send_e_mail.py
@@ -1,33 +1,20 @@
-import smtplib
-from email.mime.text import MIMEText
+import win32com.client
 
-def mail_send():
-# メール情報
-    sender = 'matsumotokeiri_hirasawa@outlook.jp'
-    receiver = '@outlook.jp'
-    subject = 'テストメール'
-    body = 'これはPythonから送信されたメールです。'
-# メール作成
-    msg = MIMEText(body, 'plain', 'utf-8')
-    msg['Subject'] = subject
-    msg['From'] = sender
-    msg['To'] = receiver
 
-# SMTPサーバへ接続
-    smtp_server = 'smtp-mail.outlook.com'
-    smtp_port = 587
-    password = 'ここにアプリパスワードを入力'  # 「アプリパスワード」を使用
+outlook = win32com.client.Dispatch('Outlook.Application')
+mail = outlook.CreateItem(0)
 
-    try:
-        with smtplib.SMTP(smtp_server, smtp_port) as server:
-            server.starttls()
-            server.login(sender, password)
-            server.send_message(msg)
-        print("メールを送信しました。")
-
-    except Exception as e:
-        print(f"エラー: メールの送信に失敗しました。 - {e}")
+mail.to = 'matsumotokeiri_hirasawa@outlook.jp'
+mail.subject = 'エラー発生メール'
+mail.bodyFormat = 1
+mail.body = '''
+これはPythonから送信されたメールです。
+'''
 
 
 if __name__ == '__main__':
-    mail_send()
+    try:
+        mail.Send()
+        print('メールを送信しました。')
+    except Exception as e:
+        print(f"エラー: メールの送信に失敗しました。 - {e}")

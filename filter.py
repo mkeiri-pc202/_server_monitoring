15665d0d6ca5dda0946fc0a8c5b88234789176fc
mkeiri-pc202 <mkeiri_pc202@outlook.jp>
2025-11-06 10:49:24 +0900

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,5 +1,9 @@
 from app import app
 
+# --------------------------
+# ✅ Flaskのfilter条件(小数点以下の表示の調整）
+# --------------------------
+
 @app.template_filter('datetime_format')
 def datetime_format(dt): 
     return dt.strftime('%Y/%m/%d %H:%M:%S')
